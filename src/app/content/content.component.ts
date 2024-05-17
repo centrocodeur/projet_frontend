c03418f340f7c0a486d3f85ec79e07c7eb4d53e3
centrocodeur <centrocodeur@gmail.com>
2024-05-17 13:03:15 +0200

    add authentication

---
+++ src/app/content/content.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {AxiosService} from "../axios.service";
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrl: './content.component.css'
+})
+export class ContentComponent {
+
+  constructor(private  axiosService: AxiosService) {
+  }
+
+  onLogin(input: any) {
+    this.axiosService.request(
+      "POST",
+      "/login",
+      {
+        login: input.login,
+        password: input.password
+      }
+
+    )
+
+  }
+}
