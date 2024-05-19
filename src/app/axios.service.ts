7b6918c802675c4b8a26374cea344f289df4fdca
centrocodeur <158089204+centrocodeur@users.noreply.github.com>
2024-05-19 19:47:22 +0200

    Update axios.service.ts

--- src/app/axios.service.ts
+++ src/app/axios.service.ts
@@ -8,7 +8,6 @@ export class AxiosService {
 
   constructor() {
     axios.defaults.baseURL = "https://joparis24backend.up.railway.app"  // for production
-    //axios.defaults.baseURL = "http://localhost:8080"   // for test
     axios.defaults.headers.post["Content-Type"]="application/json"
   }
 
